c21ff7fe9032ec0e24217740e1de2af342a3cf97
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2018-02-15 20:14:06 -0200

    x,y,w,h

--- Aula 02/programa.js
+++ Aula 02/programa.js
@@ -1,10 +1,12 @@
 document.body.style.background = "rgb(255,127,0)";
 
-window.addEventListener("click", mudarCor);
+window.addEventListener("mousemove", mudarCor);
 
 function mudarCor(girafaBebada){
+  let h = window.innerHeight;
+  let w = window.innerWidth;
   let x = girafaBebada.x;
   let y = girafaBebada.y;
-  let msg = `X: ${x}, Y: ${y}`;
+  let msg = `X: ${x}, Y: ${y}, W: ${w}, H: ${h}`;
   console.log(msg);
 }
