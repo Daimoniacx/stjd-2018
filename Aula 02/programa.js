198a9f47dc82b71b0e3e558fbf0734686bc870de
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2018-02-15 20:07:18 -0200

    evt

--- Aula 02/programa.js
+++ Aula 02/programa.js
@@ -2,6 +2,9 @@ document.body.style.background = "rgb(255,127,0)";
 
 window.addEventListener("click", mudarCor);
 
-function mudarCor(evt){
-  console.log(evt);
+function mudarCor(girafaBebada){
+  let x = girafaBebada.x;
+  let y = girafaBebada.y;
+  let msg = `X: ${x}, Y: ${y}`;
+  console.log(msg);
 }
